040519e6696542b6d82fb9fbe53e60298a99f0cd
ShxdxwyZ <ibnu.milhan@gmail.com>
2021-08-24 21:44:43 +0800

    Added the printing of our spoofed header as well as added results.json into our files. Also added some notes to help with the understanding of our code.

--- scraper.py
+++ scraper.py
@@ -2,12 +2,12 @@
 import scrapy
 from scrapy.http.request import Request
 import requests as req
+
 global stat  # realise "stat" as a global variable
 
 
 class NewSpider(scrapy.Spider):
     name = "CreepyCrawler"
-#    url = 'http://ite.edu.sg'
     url = 'https://brickset.com/sets/year-2004'  # page URL for doing GET request
     start_urls = ['https://brickset.com/sets/year-2004']
     print("Chosen URL: ", url, "\n")
@@ -21,13 +21,14 @@ class NewSpider(scrapy.Spider):
         elif r.status_code == 500:
             stat = "Internal Server Error"
         elif r.status_code == 502:
-            stat = "Service Unavailable"
+            stat = "Bad Gateway"
         else:
             continue  # a continue is needed as there are more service codes than there are stated here
 
     print("Performing GET request....\nStatus code: ", r.status_code, stat, "\n\n")  # display status code
     # Obtaining current header
     h = req.head(url)
+    # printing current header
     print("Current header:\n=======================================================")
     for x in h.headers:
         print("\t", x, ":", h.headers[x])
@@ -38,14 +39,22 @@ class NewSpider(scrapy.Spider):
         headers = {'User-Agent': "Mobile"}
         for url in self.start_urls:
             yield Request(url, headers=headers)
+        url2 = 'http://httpbin.org/headers'  # External site to retrieve our spoofed header
+        spoofed = req.get(url2, headers=headers)
+        print("Spoofed Header: (via external site http://httpbin.org/headers)\n======================================="
+              "================\n", spoofed.text,
+              "=======================================================\n\n")
+        # large print to make output in terminal to be more pleasing to the eye
 
-    def parse(self, response):
-        css_selector = 'img'
+# parse function used to retrieve/export the type of files we want
+    def parse(self, response, **kwargs):
+        css_selector = 'img'  # using variable css_selector. we will not be using xpath for this project
         for x in response.css(css_selector):
             newsel = '@src'
             yield {
-                'Image Link': x.xpath(newsel).extract_first(),
+                'Image Link': x.xpath(newsel).extract_first(),  # extracting 'img' files
             }
+        # start of 'next page function'
         page_selector = '.next a ::attr(href)'  # to recurse to next page
         next_page = response.css(page_selector).extract_first()
         if next_page:
